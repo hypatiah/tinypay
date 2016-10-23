9269dc34c08a8b23942ee38c7938d4ec139d93cb
hypatiah <danielle.hassid@gmail.com>
2016-10-23 01:58:30 -0700

    Add bootstrap gentelella template, format off

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Tinypay
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'gentelella', 'production')
   end
 end
